c6a01f39aaec9949f6dcdcf1a72ed2348361403c
hashntrash <oliverchong987@gmail.com>
2021-01-23 21:07:01 +0800

    finished project 10: grocery bud

--- 10-grocery-bud/setup/src/Alert.js
+++ 10-grocery-bud/setup/src/Alert.js
@@ -1,7 +1,19 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 
-const Alert = () => {
-  return <h2>alert component</h2>
-}
+const Alert = ({ type, msg, list, removeAlert }) => {
+  //its good to run the useEffect whenever theres an update to the state value, not just when the timeout is finished.
+  //adding the list state to the Alert Component as props and to the useEffect dependency array shows that it will run whenever there is a change in the list state.
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      removeAlert();
+    }, 3000);
 
-export default Alert
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [list]);
+
+  return <p className={`alert alert-${type}`}>{msg}</p>;
+};
+
+export default Alert;
--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -1,9 +1,127 @@
-import React, { useState, useEffect } from 'react'
-import List from './List'
-import Alert from './Alert'
+//grocery shopping list
+
+//-must have local storage, list will not dissapear when page refreshes
+//-functionality for adding, removing, removing all, and editing items in the list
+//-alert that pops up for all actions above
+
+import React, { useState, useEffect } from "react";
+import List from "./List";
+import Alert from "./Alert";
+
+// ====================================================================== LOCAL STORAGE FUNCTION ======================================================================
+const getLocalStorage = () => {
+  let list = localStorage.getItem("list");
+  if (list) {
+    return JSON.parse(localStorage.getItem("list"));
+  } else {
+    return [];
+  }
+};
+// ====================================================================== END OF LOCAL STORAGE FUNCTION ======================================================================
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  //====================================================================== ALL STATE VALUES ======================================================================
+  const [name, setName] = useState("");
+  //calling getLocalStorage() here
+  const [list, setList] = useState(getLocalStorage());
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  //the alert state is an object now because we have multiple displays showing different text.
+  const [alert, setAlert] = useState({
+    show: false,
+    type: "",
+    msg: "",
+  });
+
+  //====================================================================== END OF ALL STATE VALUES ======================================================================
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    //VALUE IS EMPTY
+    if (!name) {
+      //display alert
+      showAlert(true, "danger", "Please Enter an Item");
+    }
+    //if value is not empty, AND isEditing is true
+    else if (name && isEditing) {
+      //deal with edit
+      setList(
+        list.map((item) => {
+          if (item.id === editID) {
+            return { ...item, title: name };
+          }
+          return item;
+        })
+      );
+      setName("");
+      setEditID(null);
+      setIsEditing(false);
+      showAlert(true, "success", "value changed");
+    } else {
+      //adding a new item to the list
+      showAlert(true, "success", "Item added to the list");
+      const newItem = { id: new Date().getTime().toString(), title: name };
+      setList([...list, newItem]);
+      setName("");
+    }
+  };
+
+  //optimizing code: function showAlert that setAlert according to its arguments.
+  const showAlert = (show = false, type = "", msg = "") => {
+    setAlert({ show, type, msg });
+  };
+
+  const clearList = () => {
+    showAlert(true, "danger", "emptied list");
+    setList([]);
+  };
+
+  const removeItem = (id) => {
+    showAlert(true, "danger", "item removed");
+    setList(list.filter((item) => item.id !== id));
+  };
+
+  const editItem = (id) => {
+    const specificItem = list.find((item) => item.id === id);
+    setIsEditing(true);
+    setEditID(id);
+    setName(specificItem.title);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list));
+  }, [list]);
+
+  return (
+    <section className='section-center'>
+      <form className='grocery-form' onSubmit={handleSubmit}>
+        {/* if alert.show is true, render <Alert /> */}
+        {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
+        <h3>Grocery Buddy</h3>
+        <div className='form-control'>
+          <input
+            type='text'
+            className='grocery'
+            placeholder='e.g. Eggs'
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
+          <button type='submit' className='submit-btn'>
+            {isEditing ? "Edit" : "Submit"}
+          </button>
+        </div>
+      </form>
+      {/* below code displayed if list > 0 */}
+      {list.length > 0 && (
+        <div className='grocery-container'>
+          <List items={list} removeItem={removeItem} editItem={editItem} />
+          <button className='clear-btn' onClick={clearList}>
+            Clear Items
+          </button>
+        </div>
+      )}
+    </section>
+  );
 }
 
-export default App
+export default App;
