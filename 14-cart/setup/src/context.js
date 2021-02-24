6b8d70411daee603df01a5f1ac429c3172a4bbfe
hashntrash <oliverchong987@gmail.com>
2021-02-24 19:24:05 +0800

    completed project 14-cart

--- 14-cart/setup/src/context.js
+++ 14-cart/setup/src/context.js
@@ -1,27 +1,77 @@
-import React, { useState, useContext, useReducer, useEffect } from 'react'
-import cartItems from './data'
-import reducer from './reducer'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-useReducer-cart-project'
-const AppContext = React.createContext()
+import React, { useState, useContext, useReducer, useEffect } from "react";
+import cartItems from "./data";
+import reducer from "./reducer";
 
+const url = "https://course-api.com/react-useReducer-cart-project";
+const AppContext = React.createContext();
+
+const initialState = {
+  loading: false,
+  //cartItems is an array of objects from data.js.
+  cart: cartItems,
+  total: 0,
+  amount: 0,
+};
 const AppProvider = ({ children }) => {
-  const [cart, setCart] = useState(cartItems)
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const clearCart = () => {
+    dispatch({ type: "CLEAR_CART" });
+  };
+
+  const remove = (id) => {
+    dispatch({ type: "REMOVE", payload: id });
+  };
+
+  const increase = (id) => {
+    dispatch({ type: "INCREASE", payload: id });
+  };
+
+  const decrease = (id) => {
+    dispatch({ type: "DECREASE", payload: id });
+  };
+
+  //fetch data
+  const fetchData = async () => {
+    dispatch({ type: "LOADING" });
+    const response = await fetch(url);
+    const cart = await response.json();
+    //whatever we get back from the API, we use it as a payload for dispatch: DISPLAY_ITEMS
+    dispatch({ type: "DISPLAY_ITEMS", payload: cart });
+  };
+
+  const toggleAmount = (id, type) => {
+    dispatch({ type: "TOGGLE_AMOUNT", payload: { id, type } });
+  };
+  //we want to trigger our fetchData only when our app renders, so we set up another useEffect because there is no limit for useEffects
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  //we want to set up a useEffect, that will trigger a function call whenever the state.cart changes: this is to set up the display of the total price of all the items.
+  useEffect(() => {
+    dispatch({ type: "GET_TOTALS" });
+  }, [state.cart]);
 
   return (
     <AppContext.Provider
       value={{
-        cart,
+        //this is spreading out all the state values from reducer
+        ...state,
+        clearCart,
+        remove,
+        increase,
+        decrease,
+        toggleAmount,
       }}
     >
       {children}
     </AppContext.Provider>
-  )
-}
-// make sure use
-export const useGlobalContext = () => {
-  return useContext(AppContext)
-}
+  );
+};
 
-export { AppContext, AppProvider }
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
---
+++ 14-cart/setup/src/reducer.js
@@ -0,0 +1,91 @@
+import App from "./App";
+
+const reducer = (state, action) => {
+  if (action.type === "CLEAR_CART") {
+    return { ...state, cart: [] };
+  }
+  if (action.type === "REMOVE") {
+    return {
+      ...state,
+      cart: state.cart.filter((cartItem) => cartItem.id !== action.payload),
+    };
+  }
+
+  //THIS IS THE REFACTORING OF DECREASE AND INCREASE INTO ONE SINGLE FUNCTION, INSTEAD OF HAVING TWO DISTINCT ACTION.TYPES
+  if (action.type === "INCREASE") {
+    let tempCart = state.cart.map((cartItem) => {
+      if (cartItem.id === action.payload) {
+        return { ...cartItem, amount: cartItem.amount + 1 };
+      }
+      return cartItem;
+    });
+    return { ...state, cart: tempCart };
+  }
+
+  //for the decrease, we also want to add the functionality that if we decrease BELOW ONE, we just delete the item entirely from the cart.
+
+  if (action.type === "DECREASE") {
+    let tempCart = state.cart
+      .map((cartItem) => {
+        if (cartItem.id === action.payload) {
+          return { ...cartItem, amount: cartItem.amount - 1 };
+        }
+        return cartItem;
+      })
+      .filter((cartItem) => cartItem.amount !== 0);
+    return { ...state, cart: tempCart };
+  }
+
+  //this action type deals with the icon on the top right corner, showcasing the quantity of all the items inside the cart, and the bottom right corner, where it displays the PRICE.
+  if (action.type === "GET_TOTALS") {
+    let { total, amount } = state.cart.reduce(
+      (cartTotal, cartItem) => {
+        const { price, amount } = cartItem;
+
+        cartTotal.total += price * amount;
+        cartTotal.amount += amount;
+        return cartTotal;
+      },
+      {
+        total: 0,
+        amount: 0,
+      }
+    );
+    total = parseFloat(total.toFixed(2));
+
+    return { ...state, total, amount };
+  }
+
+  if (action.type === "LOADING") {
+    return { ...state, loading: true };
+  }
+
+  if (action.type === "DISPLAY_ITEMS") {
+    return { ...state, cart: action.payload, loading: false };
+  }
+
+  if (action.type === "TOGGLE_AMOUNT") {
+    let tempCart = state.cart
+      .map((cartItem) => {
+        //this is the first case where the cartItem.id MATCHES the action.payload.id
+        if (cartItem.id === action.payload.id) {
+          if (action.payload.type === "inc") {
+            return { ...cartItem, amount: cartItem.amount + 1 };
+          }
+          if (action.payload.type === "dec") {
+            return { ...cartItem, amount: cartItem.amount + 1 };
+          }
+        }
+        return cartItem;
+      })
+      .filter((cartItem) => cartItem.amount !== 0);
+
+    //we still set up a map for this, however in this case, we will check if we are increasing or decreasing, and then we will also change the values
+    return { ...state, cart: tempCart };
+  }
+  throw new Error("no matching action type");
+
+  return state;
+};
+
+export default reducer;
