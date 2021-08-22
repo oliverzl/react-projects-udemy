a56f61f407597803f0456a3dc6380f7a05fe1360
oliverchong97 <oliverchong987@gmail.com>
2021-08-22 23:39:36 +0800

    completed video 277, login and logout functionality: able to use Auth0 for logging in and logging out with google, created accounts and twitter.

--- Major Projects/1.SearchUsers/src/index.js
+++ Major Projects/1.SearchUsers/src/index.js
@@ -2,15 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-
 import { GithubProvider } from "./context/context";
 import { Auth0Provider } from "@auth0/auth0-react";
 
+//dev--wkmxx-3.us.auth0.com
+//MMnrSbMxPhI2QW2vcScMhYYePB5ux8Wa
+
 ReactDOM.render(
 	<React.StrictMode>
-		<GithubProvider>
-			<App />
-		</GithubProvider>
+		<Auth0Provider
+			domain="dev--wkmxx-3.us.auth0.com"
+			clientId="MMnrSbMxPhI2QW2vcScMhYYePB5ux8Wa"
+			redirectUri={window.location.origin}
+		>
+			<GithubProvider>
+				<App />
+			</GithubProvider>
+		</Auth0Provider>
 	</React.StrictMode>,
 	document.getElementById("root")
 );
